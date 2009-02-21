bb9ca832b9ec00010b8784b060aa046fb8440f86
Eric Davis <endavis@gmail.com>
2009-02-21 18:06:27 +0000

    stringfuncs.lua  - removed strip, it was already in commans

--- lua/stringfuncs.lua
+++ lua/stringfuncs.lua
@@ -4,10 +4,5 @@ http://code.google.com/p/bastmush
  - Documentation and examples
  
 functions in this module
- strip - strip a string of all forward and trailing whitespace
 --]]
  
-function strip (str)
-    s, _ =  string.gsub (str, "^%s*(.-)%s*$", "%1")
-    return s
-end
