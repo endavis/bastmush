7987f40319c6c921c0fbfc7dbf49f7d892ed3d04
Eric Davis <endavis@gmail.com>
2009-05-03 03:06:32 +0000

    stringfuncs.lua  - moved findkeyword to another module

--- Bast/lua/stringfuncs.lua
+++ Bast/lua/stringfuncs.lua
@@ -6,24 +6,3 @@ http://code.google.com/p/bastmush
 functions in this module
 --]]
  
- function findkeyword(item)
-  wlist = utils.split(item, " ")
-  badwords = {
-    ring = 1,
-    aardwolf = 1,
-    of = 1,
-    the = 1,
-    davinci = 1,
-    a = 1,
-    an = 1,
-  }
-  local name = ""
-  for i,v in ipairs(wlist) do
-    tfind = string.find(v, "'")
-    if badwords[string.lower(v)] ~= 1 and tfind == nil then
-      name = v
-      break
-    end
-  end
-  return string.lower(name)
-end
