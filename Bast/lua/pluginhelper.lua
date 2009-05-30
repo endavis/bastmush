bcd294b234592fbf1a603c4b52d5f2e04e7910bf
Eric Davis <endavis@gmail.com>
2009-05-30 20:04:38 +0000

    pluginhelper.lua  - changed the way a plugin uses pluginhelper

--- Bast/lua/pluginhelper.lua
+++ Bast/lua/pluginhelper.lua
@@ -2,41 +2,48 @@
 --[[
 http://code.google.com/p/bastmush
  - Documentation and examples
- 
+
 this module will help with setting up plugin commands and variables
 
 requires the verify module
 
-an option table looks like this
-options_table  = {
-  plotlength = {help="set the length of the moon plot", type="number", high=80, low=0, after=styleplotdata, default=66},
-  plugin_colour = {help="set the plugin colour", type="colour", default="lime"},
-  tdebug = {help="toggle this for debugging info", type="bool", default=false},
-  cmd = {help="the command to type for this plugin", type="string", after=set_plugin_alias, default="mb"},
-  three_colour = {help="the colour for the when three moons are up", type="colour", default=verify_colour("gold"), sortlev=2}  
-}
-valid values:
+adding an option looks like this
+add_option('plotlength' , {help="set the length of the moon plot", type="number", high=80, low=0, after=styleplotdata, default=66})
+
+valid values in the table are:
   help     -- the help for this option
   type     -- the type of this option, valid are string, bool, colour, number
   default  -- the default value
   high,low -- valid for numbers only, the lowest and highest values for this option
   after    -- the function to run after this option has been set
   sortlev  -- you can group options by setting this, all options with the same number will be printed together
-NOTE: the plugin_colour and cmd options are required!! 
-  
-a command table looks like this
-cmds_table = {
-  plot      = {func=plotdata, help="plot moons"},
-  union     = {func=moonbot_union, help="show next moons union"}, 
-  print     = {func=printmoons, help="print the current moon phase #s"},
-  toggle    = {func=togglewindow, help="toggle the moons miniwindow"}, 
-  reset     = {func=moonbot_reset, help="reset the moons"},
-}
+  readonly -- this is a read only variable
+
+options already included (these do not need to be manually added)
+cmd - the cmd for this plugin
+plugin_colour - the colour for this plugin
+tdebug - the debug variable
+
+to change the defaults for these options
+option_set_default('cmd', 'regen')
+option_set_default('plugin_colour', 'orange')
+
+adding a command looks like this
+add_cmd('plot', {func=plotdata, help="plot moons"})
+
 valid values -
-  func     -- the function to call 
-              the arguments are sent in this order (name, line, wildcards, cmds_table, options_table, window)
+  func     -- the function to call
+              the arguments are sent in this order (name, line, wildcards)
+              this can be set to nofunc to have this command just be a placeholder
   help     -- the help for this command
+              if this is set to "", will not show when the help prints all valid commands
   default  -- set this true and this will be the default cmd
+  send_to_world -- set this to pass this to the world
+
+commands already included (these do not need to be manually added)
+  help
+  set
+  reset
 
 --]]
 
@@ -45,6 +52,8 @@ require "commas"
 require "verify"
 require "utils"
 
+window = nil
+send_to_world = false
 
 function set_plugin_alias()
   --[[
@@ -60,36 +69,27 @@ end
 
 function plugin_header(header)
   header = header or ""
-  ColourNote("", "", "")    
+  ColourNote("", "", "")
   ColourNote(RGBColourToName(var.plugin_colour), "black", GetPluginInfo(GetPluginID (),1) .. " ",
-             RGBColourToName(var.plugin_colour), "black", GetPluginInfo(GetPluginID (),19),
-             "white", "black", header)    
-  ColourNote("white", "black", "-----------------------------------------------")   
+             RGBColourToName(var.plugin_colour), "black", GetPluginInfo(GetPluginID (),19) .. " ",
+             "white", "black", header)
+  ColourNote("white", "black", "-----------------------------------------------")
 end
 
-function plugin_help_helper(name, line, wildcards, cmds_table, options_table, window, send_to_world)
+function plugin_help_helper(name, line, wildcards)
   --[[
     this function prints a help table for cmds_table
   --]]
-  plugin_header(" Commands")
-             
+  plugin_header("Commands")
+
   for i,v in pairs(cmds_table) do
     if v.help ~= '' then
       ColourNote( "white", "black", string.format("%-15s", i),
               RGBColourToName(var.plugin_colour),  "black", ": " .. v.help )
     end
   end
-  for i,v in pairs(default_cmds_table) do
-    if v.help ~= '' then
-      ColourNote( "white", "black", string.format("%-15s", i),
-             RGBColourToName(var.plugin_colour),  "black", ": " .. v.help )
-    end
-  end
   ColourNote( "", "", "")
-  if send_to_world then
-    SendNoEcho(line)
-  end
-  
+
 end
 
 function print_setting_helper(setting, value, help, ttype)
@@ -101,7 +101,7 @@ function print_setting_helper(setting, value, help, ttype)
   if ttype == "colour" then
     local tcolour = verify_colour(value)
     if tcolour ~= nil then
-     colour = tcolour 
+     colour = tcolour
      value = RGBColourToName(colour)
     end
   end
@@ -110,49 +110,41 @@ function print_setting_helper(setting, value, help, ttype)
               "white", "black", " - " .. help)
 end
 
-function print_settings_helper(ttype, options_table, window)
+function print_settings_helper(ttype)
   --[[
     this function goes through the setoptions table and the window and prints each setting
   --]]
-  plugin_header(" Settings")
+  plugin_header("Settings")
   if ttype == "plugin" or ttype == "all" then
-    skeys = sort_settings(options_table)  
-    for _,v in ipairs(skeys) do  
-      if options_table[v].get then
-        value = options_table[v].get(v)
+    skeys = sort_settings(options_table)
+    for _,v in ipairs(skeys) do
+      local soption = find_option(v)
+      if soption.get then
+        value = soption.get(v)
       else
         value = var[v]
-      end    
-      print_setting_helper(v, value, options_table[v].help, options_table[v].type)
+      end
+      print_setting_helper(v, value, soption.help, soption.type)
     end
   end
   if ttype == "window" or ttype == "all" then
     if window then
       window:print_settings()
-    end  
+    end
   end
   ColourNote("", "", "")
 end
 
-function plugin_reset(name, line, wildcards, cmds_table, options_table, window)
+function plugin_reset(name, line, wildcards)
   if not wildcards.list then
-    print_settings_helper("plugin", options_table, window)
+    print_settings_helper("plugin")
     return
   end
   plugin_header()
   local tvar = utils.split(wildcards.list, " ")
   local ttype = tvar[1]
   if ttype == "plugin" or ttype == "all" then
-    for i,v in pairs(options_table) do  
-      test = verify(v.default, v.type, {low=v.low, high=v.high, silent=true})  
-      if test ~= nil then
-        var[i] = test
-        afterf = v.after
-        if afterf then
-          afterf()
-        end
-      end      
-    end
+    init_plugin_vars(true)
     ColourNote(RGBColourToName(var.plugin_colour), "black", "Plugin options reset ")
   end
   if ttype == "window" or ttype == "all" then
@@ -161,21 +153,21 @@ function plugin_reset(name, line, wildcards, cmds_table, options_table, window)
     end
     ColourNote(RGBColourToName(var.plugin_colour), "black", "Window options reset ")
   end
-  ColourNote("", "", "")  
+  ColourNote("", "", "")
 end
 
-function plugin_set_helper(name, line, wildcards, cmds_table, options_table, window)
+function plugin_set_helper(name, line, wildcards)
   --[[
     this function will attempt to set an item in the options_table table or in a window
   --]]
   local function nooption()
     ColourNote("", "", "")
     ColourNote("white", "black", "That is not a valid setting")
-    print_settings_helper("all", options_table, window)
+    print_settings_helper("all")
   end
-  
+
   if not wildcards.list then
-    print_settings_helper("plugin", options_table, window)
+    print_settings_helper("plugin")
     return
   end
   local tvar = utils.split(wildcards.list, " ")
@@ -184,9 +176,9 @@ function plugin_set_helper(name, line, wildcards, cmds_table, options_table, win
     option = trim(option)
   end
   if option == "all" or option == "window" then
-    print_settings_helper(option, options_table, window)
+    print_settings_helper(option)
     return
-  end  
+  end
   table.remove(tvar, 1)
   local value = table.concat(tvar, " ")
   if value ~= nil then
@@ -196,7 +188,7 @@ function plugin_set_helper(name, line, wildcards, cmds_table, options_table, win
     nooption()
     return false
   else
-    local soption = options_table[option]
+    local soption = find_option(option)
     if soption and soption.readonly then
       plugin_header()
       print("That is a read-only var")
@@ -214,11 +206,11 @@ function plugin_set_helper(name, line, wildcards, cmds_table, options_table, win
     if not f then
       f = set_var
     end
-    test = f(value, option, options_table[option].type, {low=options_table[option].low, high=options_table[option].high})  
+    test = f(value, option, soption.type, {low=soption.low, high=soption.high})
     if test == nil then
       return false
-    end    
-    afterf = options_table[option].after
+    end
+    afterf = soption.after
     if afterf then
       afterf()
     end
@@ -227,22 +219,38 @@ function plugin_set_helper(name, line, wildcards, cmds_table, options_table, win
   end
 end
 
-default_cmds_table = {
+cmds_table = {
   help      = {func=plugin_help_helper, help="show help"},
   set       = {func=plugin_set_helper, help="set script and window vars, show plugin vars when called with no arguments, 'window': show window vars, 'all': show all vars"},
   reset     = {func=plugin_reset, help="reset plugin to default values, 'all': both miniwin and plugin, 'win': just miniwin, 'plugin': just plugin"},
 }
 
-function find_cmd(cmd, cmds_table)
+options_table = {
+  plugin_colour = {help="set the plugin colour", type="colour", default="lime"},
+  tdebug = {help="toggle this for debugging info", type="bool", default=false},
+  cmd = {help="the command to type for this plugin", type="string", after=set_plugin_alias, default="mb"},
+}
+
+function find_option(option)
+  soption = options_table[option]
+  return soption
+end
+
+function add_cmd(name, stuff)
+  if not cmds_table[name] then
+    cmds_table[name] = stuff
+  else
+    print("cmd", name, "already exists")
+  end
+end
+
+function find_cmd(cmd)
   --[[
     find the cmd in the cmds and default_cmds tables
   --]]
   cmd = string.lower(cmd)
   if cmds_table[cmd] then
     return cmd, cmds_table[cmd]
-  end
-  if default_cmds_table[cmd] then
-    return cmd, default_cmds_table[cmd]
   end
   fcmd = "^" .. cmd .. ".*$"
   for tcmd,cmditem in pairs(cmds_table) do
@@ -251,18 +259,40 @@ function find_cmd(cmd, cmds_table)
       return tcmd, cmds_table[tcmd]
     end
   end
-  for tcmd,cmditem in pairs(default_cmds_table) do
-    tstart, tend =  string.find(string.lower(tcmd), fcmd)
-    if tstart and tstart > 0 then
-      return tcmd, default_cmds_table[tcmd]
-    end
-  end
-  
+
   return nil, nil
 
 end
 
-function plugin_parse_helper(name, line, wildcards, cmds_table, options_table, window, send_to_world)
+function do_cmd(tcmd, name, line, wildcards)
+  local ran = false
+  fullcmd, cmd = find_cmd(tcmd)
+  if cmd == nil then
+    if send_to_world then
+      SendNoEcho(line)
+      return true
+    else
+      ColourNote("", "", "")
+      ColourNote("white", "black", "That is not a valid command")
+      do_cmd("help", name, line, wildcards)
+    end
+    return false
+  end
+
+  if not cmd.func then
+    ColourNote("red", "", "The function for command " .. fullcmd .. " is invalid, please check plugin")
+    return false
+  end
+  if cmd.func (name, line, wildcards) then
+    ran = true
+  end -- all done
+  if cmd.send_to_world then
+    SendNoEcho(line)
+  end
+  return ran
+end
+
+function plugin_parse_helper(name, line, wildcards)
   --[[
     find the command that was specified and pass arguments to it
   --]]
@@ -274,62 +304,49 @@ function plugin_parse_helper(name, line, wildcards, cmds_table, options_table, w
       end
     end
     if wildcards.action == "" then
-      plugin_help_helper(name, line, wildcards, cmds_table, options_table, window, send_to_world) 
+      do_cmd("help", name, line, wildcards)
       return true
     end
   end
-  
-  fullcmd, cmd = find_cmd(wildcards.action, cmds_table)
-  if cmd == nil then
-    if send_to_world then
-      SendNoEcho(line)
-      return false
-    end
-    ColourNote("", "", "") 
-    ColourNote("white", "black", "That is not a valid command")
-    plugin_help_helper(name, line, wildcards, cmds_table, options_table, window, send_to_world)
-    return false
-  end
 
-
-  if not cmd.func then
-    ColourNote("red", "", "The function for command " .. fullcmd .. " is invalid, please check plugin")
-    return false
-  end
-  if cmd.func (name, line, wildcards, cmds_table, options_table, window, send_to_world) then
-    return true
-  end -- all done
-
-  return false
+  return do_cmd(wildcards.action, name, line, wildcards)
 end
 
 function set_var(value, option, type, args)
   --[[
      set a variable in a plugin, requires the "var" module
   --]]
-  local tvalue = verify(value, type, args)    
+  local tvalue = verify(value, type, args)
   if tvalue == nil then
-    ColourNote("red", "black", "That is not a valid value.")  
+    ColourNote("red", "black", "That is not a valid value.")
     return nil
   end
-  plugin_header(" Settings") 
+  plugin_header("Settings")
   if type == "colour" then
     colourname = RGBColourToName(tvalue)
     ColourNote("orange", "black", option .. " set to : ",
-             colourname, "black", colourname)    
+             colourname, "black", colourname)
   else
     colourname = RGBColourToName(var.plugin_colour)
     ColourNote("orange", "black", option .. " set to : ",
              colourname, "black", tostring(tvalue))
   end
-  ColourNote("", "", "")    
+  ColourNote("", "", "")
   var[option]= tvalue
   return true
 end
 
-function init_plugin_vars(options_table, reset)
+function add_option(name, stuff)
+  if not options_table[name] then
+    options_table[name] = stuff
+  else
+    print("option", name, "already exists")
+  end
+end
+
+function init_plugin_vars(reset)
   --[[
-    initialize all variables in the options table, requires "var" module
+    initialize all variables in the options_table, requires "var" module
   --]]
   for i,v in pairs(options_table) do
     local tvalue = nil
@@ -338,16 +355,16 @@ function init_plugin_vars(options_table, reset)
     else
       tvalue = GetVariable(i) or v.default
     end
-    tvalue = verify(tvalue, v.type, {low=v.low, high=v.high, silent=true}) 
+    tvalue = verify(tvalue, v.type, {low=v.low, high=v.high, silent=true})
     var[i] = tvalue
     afterf = v.after
     if afterf then
       afterf()
-    end    
+    end
   end
 end
 
-function plugin_save_vars(options_table)
+function plugin_save_vars()
   --[[
      save all the vars in the options table, requires the "var" module
   --]]
@@ -356,38 +373,40 @@ function plugin_save_vars(options_table)
   end
 end
 
-function sort_settings(options_table)
+function sort_settings(toptions_table)
   --[[
      sort the keys of the options table
-  --]]  
-  local function sortfunc (a, b) 
-    asortlev = options_table[a].sortlev or 50
-    bsortlev = options_table[b].sortlev or 50
+  --]]
+  local function sortfunc (a, b)
+    asortlev = toptions_table[a].sortlev or 50
+    bsortlev = toptions_table[b].sortlev or 50
     return (asortlev < bsortlev)
-  end   
+  end
 
 
   local t2 = {}
-  for i,v in pairs(options_table) do
-    table.insert(t2, i)
+  if toptions_table then
+    for i,v in pairs(toptions_table) do
+      table.insert(t2, i)
+    end
   end
   table.sort(t2, sortfunc)
-  
+
   return t2
 
 end
 
-function send_cmd_world(name, line, wildcards, cmds_table, options_table, window)
+function send_cmd_world(name, line, wildcards)
    SendNoEcho(line)
 end
 
-function togglewindow(name, line, wildcards, cmds_table, options_table, window)
-  if window ~= nil then 
+function togglewindow(name, line, wildcards)
+  if window ~= nil then
     window:togglewindow(window)
   end
 end
 
-function nofunc(name, line, wildcards, cmds_table, options_table, window)
+function nofunc(name, line, wildcards)
   return true
 end
 
@@ -395,7 +414,7 @@ function broadcast(num, data, broadcastdata)
   if var.tdebug == "true" then
     print(GetPluginInfo (GetPluginID (), 1), ": Broadcast", num)
     if data then
-      print(data)  
+      print(data)
     end
     print("")
   end
@@ -415,7 +434,7 @@ end
 
 function mdebug(...)
   if var.tdebug == "true" then
-    print(GetPluginInfo (GetPluginID (), 1), ": Debug")  
+    print(GetPluginInfo (GetPluginID (), 1), ": Debug")
     print(...)
     print(" ")
   end
@@ -444,3 +463,20 @@ function SecondsToDHMS(sSeconds)
     return nDays, nHours, nMins, nSecs
   end
 end
+
+function window_set(twindow)
+  window = twindow
+end
+
+function option_set_default(opname, opdef)
+  options_table[opname].default = opdef
+end
+
+function cmd_update(cmd, key, value)
+  cmds_table[cmd][key] = value
+end
+
+function set_send_to_world(tf)
+  send_to_world = tf
+end
+
