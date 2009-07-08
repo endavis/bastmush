26742f30a5c1b5f3318d4f923b8bbcdcba2fb8e7
Eric Davis <endavis@gmail.com>
2009-07-08 14:39:43 +0000

    sqlitedb.lua  - added warning to change dblocation variable

--- Bast/lua/sqlitedb.lua
+++ Bast/lua/sqlitedb.lua
@@ -8,14 +8,29 @@ require 'var'
 class "Sqlitedb"
 
 function Sqlitedb:initialize(args)
-  self.dbloc = GetPluginVariable ("", "dblocation") or './'
+  self.dbloc = GetPluginVariable ("", "dblocation") or GetInfo(58)
   self.db = nil
   self.dbname = "/stats.db"
   self.conns = 0
 end
 
+function Sqlitedb:checkdir()
+  if self.dbloc == nil and verify_bool(var.warned) ~= true and GetInfo(227) == 8 then
+      var.warned = true
+      SaveState()
+      ColourNote("red", "black", "---------------------- " .. GetPluginInfo (GetPluginID (), 1) .. " ----------------------")
+      ColourNote("red", "black", " please set the dblocation variable in the world")
+      ColourNote("red", "black", " to the directory that you would like the db to be saved")
+      ColourNote("red", "black", " If this is not changed, then the db will be saved in")
+      ColourNote("red", "black", " the log directory which is:")
+      ColourNote("red", "black", self.dbloc)
+      ColourNote("red", "black", " You will only see this message once per plugin.")
+      ColourNote("red", "black", "---------------------- " .. GetPluginInfo (GetPluginID (), 1) .. " ----------------------")
+  end
+end
+
 function Sqlitedb:open()
-  self.dbloc = GetPluginVariable ("", "dblocation") or './'
+  self:checkdir()
   --mdebug('open - conns:', self.conns)
   if self.db == nil then
     --mdebug("opening db")
@@ -40,7 +55,7 @@ end
 
 function Sqlitedb:checkfortable(tablename)
   if self:open() then
-    for a in self.db:nrows('SELECT name FROM sqlite_master') do
+    for a in self.db:nrows('SELECT * FROM sqlite_master WHERE name = "' .. tablename .. '" AND type = "table"') do
       if a['name'] == tablename then
         self:close()
         return true
