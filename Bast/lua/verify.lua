c3e43ca051bedd6444a76a6c12cbd8b31542f986
Eric Davis <endavis@gmail.com>
2009-07-13 18:50:52 +0000

    verify.lua  - fixed a bug in an error message  - no longer an crashes when type is nil when calling verify

--- Bast/lua/verify.lua
+++ Bast/lua/verify.lua
@@ -2,7 +2,7 @@
 --[[
 http://code.google.com/p/bastmush
  - Documentation and examples
- 
+
 the following functions can be used to verify data, if the data is not valid for that type, nil is returned
 
 verify_colour
@@ -15,7 +15,7 @@ verify_colour
     arguments with " or ' in them are automatically invalid
   stuff to put in args table
     silent - if true, will not show PickColour dialog
-    
+
 verify_string
   Accepts
     anything that can be turned into a string
@@ -27,13 +27,13 @@ verify_number
     low  - the lowest value the data can be
     high - the highest value the data can be
     silent - if true, will not print any errors
-    
+
 verify_bool
   Accepts
     booleans
     the string true, on and the number 1 will convert to the boolean true
     the string false, off and the number 0 will convert to the boolean false
-    
+
 --]]
 
 --- verify the colour
@@ -42,7 +42,7 @@ function verify_colour(colour, args)
   args = args or {}
   local low = 0
   local high = 16777215
-    
+
   -- if no argument and not silent then open PickColour Dialog
   if (colour == nil or colour == "") and not args.silent then
     colour = PickColour(0)
@@ -50,14 +50,14 @@ function verify_colour(colour, args)
       return colour
     end
   end
-  
+
   -- test if we have the chars ' or "
   local ttest = string.find(colour, '"')
   local ttest2 = string.find(colour, "'")
   if ttest ~= nil or ttest2 ~= nil then
     return nil
   end
-  
+
   -- see if it is a number
   local tcolour = tonumber(colour)
   if tcolour and tcolour >= 0 then
@@ -65,19 +65,19 @@ function verify_colour(colour, args)
       return tcolour
     end
   else
-  
+
   -- see if it is a colour name, which also works with the #000000 format
     tcolour = ColourNameToRGB (colour)
     if tcolour ~= -1 then
        return tcolour
     end
   end
-    
+
   -- check to see if there is a colour of that name in the window
   if args.window and args.window:get_colour(colour, nil, true) ~= nil then
       return colour
   end
-    
+
   return nil
 end
 
@@ -93,10 +93,10 @@ function verify_number(numberval, args)
   args = args or {}
   -- turn it into a number
   tvalue = tonumber(numberval)
-  
+
   -- check if we were successful
   if tvalue then
-  
+
     -- check if it is lesser than the low argument
     if args.low and tvalue < args.low then
       if not args.silent then
@@ -104,18 +104,18 @@ function verify_number(numberval, args)
       end
       return nil
     end
-    
+
     -- check if is greater than the high argument
     if args.high and tvalue > args.high then
       if not args.silent then
         ColourNote("red", "white", "Value must be lower than " .. args.high)
       end
-      return nil   
+      return nil
     end
-    
+
     return tonumber(numberval)
   end
-  
+
   return nil
 end
 
@@ -125,7 +125,7 @@ function verify_bool(boolval, args)
   if type(boolval) == "boolean" then
     return boolval
   end
-  
+
   -- check to see if we have a 1 or 0
   tvalue = tonumber(boolval)
   if tvalue == 1 then
@@ -133,7 +133,7 @@ function verify_bool(boolval, args)
   elseif tvalue == 0 then
     return false
   end
-  
+
   -- check if we have the string forms
   tvalue = tostring(boolval)
   if string.lower(tvalue) == "true" or string.lower(tvalue) == "on" then
@@ -141,7 +141,7 @@ function verify_bool(boolval, args)
   elseif string.lower(tvalue) == "false" or string.lower(tvalue) == "off" then
     return false
   end
-  
+
   return nil
 end
 
@@ -153,10 +153,14 @@ verify_table = {
               }
 
 
-function verify(value, type, args)
-  f = verify_table[type]
+function verify(value, ttype, args)
+  if ttype == nil then
+    ColourNote("red", "", "Type is nil for value" .. tostring(value))
+    return
+  end
+  f = verify_table[ttype]
   if not f then
-    ColourNote("red", "", "Not a valid option type for " .. option)
+    ColourNote("red", "", "Not a valid option type for " .. value)
     return nil
   end
   return f(value, args)
