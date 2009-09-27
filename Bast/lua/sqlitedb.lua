369b50a2d2a21282885f69ffba8f6b03c7aacff8
Eric Davis <endavis@gmail.com>
2009-09-27 19:06:45 +0000

    sqlitedb.lua  - default data dir is now log dir

--- Bast/lua/sqlitedb.lua
+++ Bast/lua/sqlitedb.lua
@@ -14,26 +14,11 @@ function Sqlitedb:initialize(args)
   self.conns = 0
 end
 
-function Sqlitedb:checkdir()
-  if self.dbloc == nil and verify_bool(var.warned) ~= true and GetInfo(227) == 8 then
-      var.warned = true
-      SaveState()
-      ColourNote("red", "black", "---------------------- " .. GetPluginInfo (GetPluginID (), 1) .. " ----------------------")
-      ColourNote("red", "black", " please set the dblocation variable in the world")
-      ColourNote("red", "black", " to the directory that you would like the db to be saved")
-      ColourNote("red", "black", " If this is not changed, then the db will be saved in")
-      ColourNote("red", "black", " the log directory which is:")
-      ColourNote("red", "black", self.dbloc)
-      ColourNote("red", "black", " You will only see this message once per plugin.")
-      ColourNote("red", "black", "---------------------- " .. GetPluginInfo (GetPluginID (), 1) .. " ----------------------")
-  end
-end
-
 function Sqlitedb:open()
-  self:checkdir()
-  --mdebug('open - conns:', self.conns)
+  --phelper:mdebug('open - conns:', self.conns)
   if self.db == nil then
-    --mdebug("opening db")
+    --phelper:mdebug("opening db")
+    print('db dir', self.dbloc)
     self.db = assert(sqlite3.open(self.dbloc .. self.dbname))
   end
   self.conns = self.conns + 1
@@ -45,9 +30,9 @@ function Sqlitedb:close()
   if self.conns < 0 then
     print("BUG: conns < 0 for db", self.dbname)
   end
-  --mdebug('close - conns:', self.conns)
+  --phelper:mdebug('close - conns:', self.conns)
   if self.db ~= nil and self.conns == 0 then
-    --mdebug("closing db")
+    --phelper:mdebug("closing db")
     self.db:close()
     self.db = nil
   end
