b8fb7f5e63f846948af40352c08d68bbb1493c4d
Eric Davis <endavis@gmail.com>
2009-09-27 19:04:26 +0000

    stringfuncs.lua  - added a wrap function

--- Bast/lua/stringfuncs.lua
+++ Bast/lua/stringfuncs.lua
@@ -5,4 +5,23 @@ http://code.google.com/p/bastmush
  
 functions in this module
 --]]
- 
+
+function wrap(line, length)
+  local lines = {}
+  while #line > length do
+    -- find a space not followed by a space, or a , closest to the end of the line
+    local col = string.find (line:sub (1, length), "[%s,][^%s,]*$")
+    if col and col > 2 then
+--      col = col - 1  -- use the space to indent
+    else
+      col = length  -- just cut off at wrap_column
+    end -- if
+
+    table.insert(lines, line:sub (1, col))
+    line = line:sub (col + 1)
+
+  end
+  table.insert(lines, line)
+  return lines
+end
+
