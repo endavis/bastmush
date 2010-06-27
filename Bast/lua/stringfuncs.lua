de74246e367eabe4332dbaeb20f6fc8f96918af5
Eric Davis <endavis@gmail.com>
2010-06-27 19:09:12 +0000

    stringfuncs.lua  - added a strjoin function to join a table with a delimiter

--- Bast/lua/stringfuncs.lua
+++ Bast/lua/stringfuncs.lua
@@ -28,3 +28,14 @@ function wrap(line, length)
   return lines
 end
 
+function strjoin(delimiter, list)
+  local len = #list
+  if len == 0 then 
+    return "" 
+  end
+  local string = list[1]
+  for i = 2, len do 
+    string = string .. delimiter .. list[i] 
+  end
+  return string
+end
